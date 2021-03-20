4a5ed7d99c88e0ad1e462402b9f5a5217d4f022b
Wilson Tseng <p780729@hotmail.com>
2021-03-20 23:22:10 +0800

    串接金流 (1): GET /order/:id/payment

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,5 @@
 const db = require('../models')
+const crypto = require('crypto')
 const nodemailer = require('nodemailer')
 const Order = db.Order
 const OrderItem = db.OrderItem
@@ -16,6 +17,84 @@ const transporter = nodemailer.createTransport({
   //   rejectUnauthorized: false
   // }
 })
+
+const URL = ''
+const MerchantID = ''
+const HashKey = ''
+const HashIV = ''
+const PayGateWay = 'https://ccore.spgateway.com/MPG/mpg_gateway'
+const ReturnURL = URL + '/spgateway/callback?from=ReturnURL'
+const NotifyURL = URL + '/spgateway/callback?from=NotifyURL'
+const ClientBackURL = URL + '/orders'
+
+function genDataChain(TradeInfo) {
+  let results = []
+  for (let kv of Object.entries(TradeInfo)) {
+    results.push(`${kv[0]}=${kv[1]}`)
+  }
+  return results.join('&')
+}
+
+function create_mpg_aes_encrypt(TradeInfo) {
+  let encrypt = crypto.createCipheriv('aes256', HashKey, HashIV)
+  let enc = encrypt.update(genDataChain(TradeInfo), 'utf8', 'hex')
+  return enc + encrypt.final('hex')
+}
+
+function create_mpg_sha_encrypt(TradeInfo) {
+
+  let sha = crypto.createHash('sha256');
+  let plainText = `HashKey=${HashKey}&${TradeInfo}&HashIV=${HashIV}`
+
+  return sha.update(plainText).digest('hex').toUpperCase();
+}
+
+function getTradeInfo(Amt, Desc, email) {
+
+  console.log('===== getTradeInfo =====')
+  console.log(Amt, Desc, email)
+  console.log('==========')
+
+  const data = {
+    'MerchantID': MerchantID, // 商店代號
+    'RespondType': 'JSON', // 回傳格式
+    'TimeStamp': Date.now(), // 時間戳記
+    'Version': 1.5, // 串接程式版本
+    'MerchantOrderNo': Date.now(), // 商店訂單編號
+    'LoginType': 0, // 智付通會員
+    'OrderComment': 'OrderComment', // 商店備註
+    'Amt': Amt, // 訂單金額
+    'ItemDesc': Desc, // 產品名稱
+    'Email': email, // 付款人電子信箱
+    'ReturnURL': ReturnURL, // 支付完成返回商店網址
+    'NotifyURL': NotifyURL, // 支付通知網址/每期授權結果通知
+    'ClientBackURL': ClientBackURL, // 支付取消返回商店網址
+  }
+
+  console.log('===== getTradeInfo: data =====')
+  console.log(data)
+
+  let mpg_aes_encrypt = create_mpg_aes_encrypt(data)
+  let mpg_sha_encrypt = create_mpg_sha_encrypt(mpg_aes_encrypt)
+
+  console.log('===== getTradeInfo: mpg_aes_encrypt, mpg_sha_encrypt =====')
+  console.log(mpg_aes_encrypt)
+  console.log(mpg_sha_encrypt)
+
+  const tradeInfo = {
+    'MerchantID': MerchantID, // 商店代號
+    'TradeInfo': mpg_aes_encrypt, // 加密後參數
+    'TradeSha': mpg_sha_encrypt,
+    'Version': 1.5, // 串接程式版本
+    'PayGateWay': PayGateWay,
+    'MerchantOrderNo': data.MerchantOrderNo,
+  }
+
+  console.log('===== getTradeInfo: tradeInfo =====')
+  console.log(tradeInfo)
+
+  return tradeInfo
+}
 
 let orderController = {
   getOrders: (req, res) => {
@@ -100,7 +179,8 @@ let orderController = {
 
     return Order.findByPk(req.params.id, {}).then(order => {
       order = order.dataValues
-      return res.render('payment', { order })
+      const tradeInfo = getTradeInfo(order.amount, '產品名稱', 'shopping_cart_express@gmail.com')
+      return res.render('payment', { order, tradeInfo })
     })
   },
 
@@ -109,7 +189,7 @@ let orderController = {
     console.log(req.body)
     console.log('==========')
 
-    return res.redirect('back')
+    return res.redirect('/orders')
   }
 }
 
